ba744b7146267ba2d6c57b934273c5ecbac2e7f7
Camille PD <52207575+cpreeldumas@users.noreply.github.com>
2024-04-03 13:06:47 -0400

    moved background image to css, cleaned class names, and added container with borscht recipe

--- scripts.js
+++ scripts.js
@@ -3,43 +3,48 @@ $('.card').on('click', function () {
 
     $('#column-right').empty()
 
-if (topic === 'Seasonality') {
-$('#column-right').css('background-image', 'url("https://onwisconsin.uwalumni.com/content/uploads/2023/05/beets.png")');
+    if (topic === 'Seasonality') {
+        $('#column-right').css('background-image', 'url("https://onwisconsin.uwalumni.com/content/uploads/2023/05/beets.png")');
 
-// Adding a text element
-$('#column-right').append('<p id="additional-text">Mid-summer<br>through late fall.</p>');
+        // Adding a text element
+        $('#column-right').append('<p id="additional-text">Mid-summer<br>through late fall.</p>');
 
-// Adding CSS styling to make the text white
-$('#additional-text').css({
-'color': 'white',
-'text-shadow': '1px 1px 2px black'
-});
-}
+        // Adding CSS styling to make the text white
+        $('#additional-text').css({
+            'color': 'white',
+            'text-shadow': '1px 1px 2px black'
+        });
+    }
 
     if (topic === 'Taste') {
-$('#column-right').css('background-image', 'url("https://freshunlimited.in/cdn/shop/products/Beetroot_578x.png?v=1609401234")');
+        $('#column-right').css('background-image', 'url("https://freshunlimited.in/cdn/shop/products/Beetroot_578x.png?v=1609401234")');
 
-// Adding a text element
-$('#column-right').append('<p id="additional-text">Earthy, sweet, and<br>slightly bitter.</p>');
+        // Adding a text element
+        $('#column-right').append('<p id="additional-text">Earthy, sweet, and<br>slightly bitter.</p>');
 
-// Adding CSS styling to make the text white
-$('#additional-text').css({
-'color': 'white',
-'text-shadow': '1px 1px 2px black'
-});
-}
+        // Adding CSS styling to make the text white
+        $('#additional-text').css({
+            'color': 'white',
+            'text-shadow': '1px 1px 2px black'
+        });
+    }
 
     if (topic === 'Usage') {
-$('#column-right').css('background-image', 'url("https://cdn.shopify.com/s/files/1/0560/0732/7881/files/Ingredients_Beetroot-Powder-Detail-1.png")');
+        $('#column-right').css('background-image', 'url("https://cdn.shopify.com/s/files/1/0560/0732/7881/files/Ingredients_Beetroot-Powder-Detail-1.png")');
 
-// Adding a text element with a styled link to a website when clicking on "borscht"
-$('#column-right').append('<p id="additional-text">Make <a href="https://natashaskitchen.com/classic-russian-borscht-recipe/">borscht</a>.<br>Or roast them.<br>Avoid beet hummus.</p>');
+        // Adding a text element with a styled link to a website when clicking on "borscht"
+        $('#column-right').append('<p id="additional-text">Make <a id="borscht-link" href="#">borscht</a>.<br>Or roast them.<br>Avoid beet hummus.</p>');
 
+        // Adding CSS styling to make the text white
+        $('#additional-text').css({
+            'color': 'white',
+            'text-shadow': '1px 1px 2px black'
+        });
 
-// Adding CSS styling to make the text white
-$('#additional-text').css({
-'color': 'white',
-'text-shadow': '1px 1px 2px black'
-});
-}
+        // Toggle visibility of recipe container when clicking on "Borscht" link
+        $('#borscht-link').click(function (event) {
+            event.preventDefault(); // Prevent default link behavior
+            $('#recipe-container').toggle();
+        });
+    }
 })
